ffca0bc4ec5922ae5c7a01f598374f47b56376cc
abhay-tank <abhay.tank@raweng.com>
2020-12-05 00:26:22 +0530

    Removed comments

--- src/app.js
+++ src/app.js
@@ -3,17 +3,17 @@ import { createEmployeeMarkup } from "./helper/createEmployeeCard.js";
 import { createCompanyCard } from "./helper/createFooterCard.js";
 let employeeData;
 window.onload = () => {
-  // const parentContainer = document.getElementById("employeeDataContainer");
-  // employeeData = fetchEmployeesData();
-  // employeeData
-  //   .then((employees) => {
-  //     employees.forEach((employee) => {
-  //       parentContainer.appendChild(createEmployeeMarkup(employee));
-  //     });
-  //     parentContainer.appendChild(createCompanyCard());
-  //     document.getElementById("loadingDiv").style.display = "none";
-  //   })
-  //   .catch((err) => {
-  //     throw new Error(err);
-  //   });
+  const parentContainer = document.getElementById("employeeDataContainer");
+  employeeData = fetchEmployeesData();
+  employeeData
+    .then((employees) => {
+      employees.forEach((employee) => {
+        parentContainer.appendChild(createEmployeeMarkup(employee));
+      });
+      parentContainer.appendChild(createCompanyCard());
+      document.getElementById("loadingDiv").style.display = "none";
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 };
